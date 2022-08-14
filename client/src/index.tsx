436a33e93a718488cb424f3087bc1cfe9f98cf3e
Gaspar Chefdeville <gaspar.chefdeville@gmail.com>
2022-08-14 02:55:55 +0200

    feat(client): reinitialize css

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import store from './store/store'
 import theme from './theme'
+import './index.css'
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
