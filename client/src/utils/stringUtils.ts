9a26921d72307f0fb1f899b65a0721abea490475
Gaspar Chefdeville <gaspar.chefdeville@gmail.com>
2022-08-14 20:23:01 +0200

    feat(client): create IconTooltip component

--- client/src/utils/stringUtils.ts
+++ client/src/utils/stringUtils.ts
@@ -64,3 +64,15 @@ export const assertIsValidHex = (hex: string): void => {
 		throw new Error('Invalid hex color')
 	}
 }
+
+/**
+ * Generate a random id, in base 36.
+ * The id can be prefixed with a string.
+ * If so, an hyphen will be inserted between the prefix and the id.
+ * @param prefix The prefix to add to the id.
+ * @returns The generated id.
+ */
+export const generateId = (prefix?: string): string => {
+	const id = Math.random().toString(36).slice(2)
+	return prefix != null ? `${prefix}-${id}` : id
+}
