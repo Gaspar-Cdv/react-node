fa638b5af976ff931b33c06fae92eeca015ed008
Gaspar Chefdeville <gaspar.chefdeville@gmail.com>
2022-08-15 12:54:37 +0200

    lib(client): install and configure react-custom-scrollbars

---
+++ client/src/common/Scrollbar.tsx
@@ -0,0 +1,22 @@
+import { forwardRef, ReactNode } from 'react'
+import { Scrollbars } from 'react-custom-scrollbars'
+
+export interface ScrollbarProps {
+	maxHeight: number
+	className?: string
+	children: ReactNode | ReactNode[]
+}
+
+const Scrollbar = forwardRef(({ maxHeight, className, children }: ScrollbarProps, ref: React.Ref<HTMLDivElement>) => {
+	return (
+		<div className={className} onClick={e => e.stopPropagation()} ref={ref}>
+			<Scrollbars autoHeight autoHeightMax={maxHeight}>
+				{children}
+			</Scrollbars>
+		</div>
+	)
+})
+
+Scrollbar.displayName = 'Scrollbar'
+
+export default Scrollbar
