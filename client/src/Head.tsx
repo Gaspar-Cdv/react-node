5355e40b0f1d1f7eef3e01cafc70060af49d6488
Gaspar Chefdeville <gaspar.chefdeville@gmail.com>
2022-08-19 22:23:47 +0200

    feat(client): add page title

--- client/src/Head.tsx
+++ client/src/Head.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet'
 import { useSelector } from 'react-redux'
+import { useCurrentTitle } from './common/routing/hooks'
 import { defineI18n, useTranslate } from './i18n'
 import { languageSelector } from './store/language/selectors'
 
@@ -17,11 +18,17 @@ const i18n = defineI18n({
 function Head () {
 	const translate = useTranslate()
 	const language = useSelector(languageSelector)
+	const pageTitle = useCurrentTitle()
+
+	const title = [
+		translate(i18n.title),
+		pageTitle
+	].filter(Boolean).join(' - ')
 
 	return (
 		<Helmet htmlAttributes={{ lang: language }}>
 			<meta charSet='UTF-8' />
-			<title>{translate(i18n.title)}</title>
+			<title>{title}</title>
 			<meta httpEquiv='X-UA-Compatible' content='IE=edge' />
 			<meta name='language' content={language} />
 			<meta name='description' content={translate(i18n.description)} />
