a0fb128af3f4b07a45028a07248c6a0fb52f37f3
Gaspar Chefdeville <gaspar.chefdeville@gmail.com>
2022-09-03 19:01:34 +0200

    feat: move validation service in common using Yup

--- server/src/utils/stringUtils.ts
+++ server/src/utils/stringUtils.ts
@@ -1,23 +1,3 @@
 export const getType = (object: any): string => {
 	return Object.prototype.toString.call(object).slice(8, -1).toLowerCase()
 }
-
-export const isEmailValid = (email: string): boolean => {
-	return /^([a-z0-9]+(?:[._-][a-z0-9]+)*)@([a-z0-9]+(?:[.-][a-z0-9]+)*\.[a-z]{2,})$/i.test(email)
-}
-
-export const isPasswordStrongEnough = (password: string, hardcheck = false): boolean => {
-	// Handle diacritics in regex
-	const MINIMUM_LENGTH = hardcheck ? 12 : 8
-	const MINIMUM_TYPES = hardcheck ? 4 : 3
-
-	const isLongEnough = password.length >= MINIMUM_LENGTH
-	const hasEnoughTypes = [
-		/[a-z]/.test(password),
-		/[A-Z]/.test(password),
-		/[0-9]/.test(password),
-		/[^a-zA-Z0-9]/.test(password),
-	].filter(Boolean).length >= MINIMUM_TYPES
-
-	return isLongEnough && hasEnoughTypes
-}
--- server/tests/unit/utils.test.ts
+++ server/tests/unit/utils.test.ts
@@ -1,6 +1,5 @@
 import chai from 'chai'
 import { JSONValidator } from '../../src/utils/JSONValidator'
-import { isEmailValid, isPasswordStrongEnough } from '../../src/utils/stringUtils'
 
 const { expect } = chai
 
@@ -45,92 +44,5 @@ describe('JSONValidator', () => {
 		expect(nestedValidator.findMissingProperties({ string, x: { y: { boolean } } })).to.eql(['x.number'])
 		expect(nestedValidator.findMissingProperties({ x: { number, y: { boolean } } })).to.eql(['string'])
 		expect(nestedValidator.findMissingProperties({ string, x: { number, y: { boolean } } })).to.eql([])
-	})
-})
-
-describe('stringUtils', () => {
-	it('should verify if email is valid', () => {
-		const correctEmails = [
-			'test@test.com',
-			'test.test@test.com',
-			'test@test.test.com',
-			'test-test@test.com',
-			'test@test-test.com',
-			'test123@test.com',
-			'test@test123.com',
-			'TEST@test.com',
-			'test@TEST.com',
-			'test@test.COM'
-		]
-
-		const incorrectEmails = [
-			'test',
-			'test@test',
-			'test.com',
-			'@test.com',
-			'test@.com',
-			'test@test.',
-			'.test@test.com',
-			'-test@test.com',
-			'test.@test.com',
-			'test-@test.com',
-			'test@.test.com',
-			'test@-test.com',
-			'test@test..com',
-			'test@test-.com',
-			'test@test.-com',
-			'test@test.com.',
-			'test@test.com-',
-			'test@test@test.com'
-		]
-
-		const toDispatch = [ // TODO are these emails valid?
-			'123@test.com',
-			'test@123.com',
-			'test@test.123'
-		]
-
-		expect(correctEmails.every(email => isEmailValid(email))).to.be.true
-		expect(incorrectEmails.every(email => !isEmailValid(email))).to.be.true
-	})
-
-	it('should verify if password is strong enough', () => {
-		const softCorrectPasswords = [
-			'abcABC123',
-			'abcABC.!?',
-			'abc123.!?',
-			'ABC123.!?',
-			'abAB12.!?'
-		]
-
-		const hardCorrectPasswords = [
-			'abcABC123.!?'
-		]
-
-		const incorrectPasswords = [
-			'abcdefgh',
-			'ABCDEFGH',
-			'12345678',
-			'.!?@/-_()',
-			'abcdABCD',
-			'abcd1234',
-			'abcd.!?@',
-			'ABCD1234',
-			'ABCD.!?@',
-			'1234.!?@',
-			'abAB12!'
-		]
-
-		const toDispatch = [ // TODO handle diacritics
-			'àéôABC123'
-		]
-
-		expect(softCorrectPasswords.every(password => isPasswordStrongEnough(password))).to.be.true
-		expect(hardCorrectPasswords.every(password => isPasswordStrongEnough(password))).to.be.true
-		expect(incorrectPasswords.every(password => !isPasswordStrongEnough(password))).to.be.true
-
-		expect(hardCorrectPasswords.every(password => isPasswordStrongEnough(password, true))).to.be.true
-		expect(softCorrectPasswords.every(password => !isPasswordStrongEnough(password, true))).to.be.true
-		expect(incorrectPasswords.every(password => !isPasswordStrongEnough(password, true))).to.be.true
 	})
 })
