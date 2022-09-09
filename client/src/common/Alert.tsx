226242ac2f32f4abfd23c515470919a42c5b1d05
Gaspar Chefdeville <gaspar.chefdeville@gmail.com>
2022-09-09 23:15:54 +0200

    feat(client): add close button on Alert component

--- client/src/common/Alert.tsx
+++ client/src/common/Alert.tsx
@@ -2,14 +2,19 @@ import classNames from 'classnames'
 import { ReactNode } from 'react'
 import { createUseStyles } from 'react-jss'
 import { ReactComponent as InfoIcon } from '../images/icons/info.svg'
+import { ReactComponent as CloseIcon } from '../images/icons/close.svg'
 
 const useStyles = createUseStyles(theme => ({
 	container: {
+		position: 'relative',
 		display: 'flex',
 		alignItems: 'center',
 		gap: 20,
-		padding: [10, 20],
-		borderRadius: theme.borderRadius.xs
+		padding: [10, 40, 10, 20],
+		borderRadius: theme.borderRadius.xs,
+		'& svg': {
+			flex: [0, 0, 'auto']
+		}
 	},
 	success: {
 		color: theme.color.success,
@@ -25,16 +30,23 @@ const useStyles = createUseStyles(theme => ({
 		color: theme.color.danger,
 		backgroundColor: theme.color.red[50],
 		border: [1, 'solid', theme.color.red[300]]
+	},
+	close: {
+		position: 'absolute',
+		top: 10,
+		right: 10,
+		cursor: 'pointer'
 	}
 }))
 
-interface AlertProps {
+export interface AlertProps {
 	show?: boolean
 	severity?: 'success' | 'warning' | 'danger'
+	onClose?: () => void
 	children: ReactNode
 }
 
-function Alert ({ show = true, severity = 'danger', children }: AlertProps) {
+function Alert ({ show = true, severity = 'danger', onClose, children }: AlertProps) {
 	const classes = useStyles()
 
 	if (!show) {
@@ -43,6 +55,9 @@ function Alert ({ show = true, severity = 'danger', children }: AlertProps) {
 
 	return (
 		<div className={classNames(classes.container, classes[severity])}>
+			{onClose != null && (
+				<CloseIcon onClick={onClose} className={classes.close} />
+			)}
 			<InfoIcon />
 			{children}
 		</div>
