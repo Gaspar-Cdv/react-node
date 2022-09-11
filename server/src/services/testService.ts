111fd53ceef51510d5d16ad1649bec03ae73994f
Gaspar Chefdeville <gaspar.chefdeville@gmail.com>
2022-09-11 18:24:01 +0200

    test(server): create testService

---
+++ server/src/services/testService.ts
@@ -0,0 +1,73 @@
+import { User } from '@prisma/client'
+import { Language } from '@title/common/build/types/Language'
+import { RegisterRequest } from '@title/common/build/types/requests/auth'
+import chai from 'chai'
+import chaiHttp from 'chai-http'
+import jwt from 'jsonwebtoken'
+import app from '..'
+import { JWT_SECRET } from '../config/environment'
+import authService from './authService'
+
+chai.use(chaiHttp)
+
+export const TEST_PASSWORD = 'Password@123'
+
+class TestService {
+	static instance: TestService
+
+	/**
+	 * Generate a register request with random and unique username and email.\
+	 * The values can be overwritten in the registerRequest argument.
+	 */
+	generateRegisterRequest = (registerRequest?: Partial<RegisterRequest>): RegisterRequest => {
+		const username = `test+${Date.now()}`
+		const email = `${username}@test.com`
+		const password = TEST_PASSWORD
+
+		return {
+			username,
+			email,
+			password,
+			passwordConfirmation: password,
+			language: Language.en,
+			...registerRequest
+		}
+	}
+
+	/**
+	 * Register a test user with random and unique username and email.\
+	 * The values can be overwritten in the registerRequest argument.
+	 */
+	createTestUser = async (registerRequest?: Partial<RegisterRequest>): Promise<User> => {
+		const data = this.generateRegisterRequest(registerRequest)
+
+		return authService.register(data)
+	}
+
+	/**
+	 * Call the endpoint of the api with the given data.\
+	 * If userId is given, a token will be generated and added to the headers of the request.
+	 */
+	call = async (endpoint: string, data: object, userId?: number) => {
+		const request = chai.request(app).post(endpoint)
+
+		if (userId != null) {
+			const token = jwt.sign({ userId }, JWT_SECRET)
+			request.set({ Authorization: `Bearer ${token}` })
+		}
+
+		return request.send(data)
+	}
+
+	/* STATIC */
+
+	static getInstance (): TestService {
+		if (!TestService.instance) {
+			TestService.instance = new TestService()
+		}
+
+		return TestService.instance
+	}
+}
+
+export default TestService.getInstance()
--- server/tests/api/auth.test.ts
+++ server/tests/api/auth.test.ts
@@ -1,44 +1,46 @@
 import { ErrorMessage } from '@title/common/build/types/ErrorMessage'
 import { Language } from '@title/common/build/types/Language'
+import { LoginRequest, RegisterRequest } from '@title/common/build/types/requests/auth'
 import chai from 'chai'
 import chaiHttp from 'chai-http'
-import app from '../../src'
 import userDao from '../../src/dao/userDao'
 import { prisma } from '../../src/prisma'
-import authService from '../../src/services/authService'
+import testService, { TEST_PASSWORD } from '../../src/services/testService'
 
 const { expect } = chai
 chai.use(chaiHttp)
 
-const STRONG_PASSWORD = 'Password@123'
-const WEAK_PASSWORD = 'pwd'
+const USERNAME = 'authTest'
+const EMAIL = 'authTest@test.com'
+
+const register = async (registerRequest: Partial<RegisterRequest>) => {
+	const request = testService.generateRegisterRequest(registerRequest)
+	return testService.call('/api/auth/register', request)
+}
+
+const login = async (loginRequest: LoginRequest) => {
+	return testService.call('/api/auth/login', loginRequest)
+}
 
 describe('register', () => {
 	after(async () => {
-		await prisma.user.deleteMany({ where: { username: 'authTest' } })
+		await prisma.user.deleteMany({ where: { username: USERNAME } })
 	})
 
 	it('should register a new user', async () => {
-		expect(await userDao.findByUsername('authTest')).to.be.null
+		expect(await userDao.findByUsername(USERNAME)).to.be.null
 
-		const res = await chai.request(app)
-			.post('/api/auth/register')
-			.send({
-				username: 'authTest',
-				email: 'authTest@test.com',
-				password: STRONG_PASSWORD,
-				passwordConfirmation: STRONG_PASSWORD
-			})
+		const res = await register({
+			username: USERNAME,
+			email: EMAIL
+		})
 
 		expect(res.status).to.equal(201)
-		expect(await userDao.findByUsername('authTest')).not.to.be.null
+		expect(await userDao.findByUsername(USERNAME)).not.to.be.null
 	})
 
 	it('should not register a new user if one of the fields is missing', async () => {
-		const username = 'authTest2'
-		const email = 'authTest@test.com'
-		const password = STRONG_PASSWORD
-		const passwordConfirmation = STRONG_PASSWORD
+		const { username, email, password, passwordConfirmation } = testService.generateRegisterRequest()
 
 		const requests = [
 			{ username, email, password },
@@ -48,9 +50,7 @@ describe('register', () => {
 		]
 
 		for (const request of requests) {
-			const res = await chai.request(app)
-				.post('/api/auth/register')
-				.send(request)
+			const res = await testService.call('/api/auth/register', request)
 
 			expect(res.status).to.equal(422)
 			expect(res.body.message).to.equal(ErrorMessage.INVALID_VALUES)
@@ -59,14 +59,7 @@ describe('register', () => {
 	})
 
 	it('should not register a new user with an existing username', async () => {
-		const res = await chai.request(app)
-			.post('/api/auth/register')
-			.send({
-				username: 'authTest',
-				email: 'authTest2@test.com',
-				password: STRONG_PASSWORD,
-				passwordConfirmation: STRONG_PASSWORD
-			})
+		const res = await register({ username: USERNAME })
 
 		expect(res.status).to.equal(422)
 		expect(res.body.message).to.equal(ErrorMessage.USERNAME_ALREADY_USED)
@@ -74,14 +67,7 @@ describe('register', () => {
 	})
 
 	it('should not register a new user with an existing email', async () => {
-		const res = await chai.request(app)
-			.post('/api/auth/register')
-			.send({
-				username: 'authTest2',
-				email: 'authTest@test.com',
-				password: STRONG_PASSWORD,
-				passwordConfirmation: STRONG_PASSWORD
-			})
+		const res = await register({ email: EMAIL })
 
 		expect(res.status).to.equal(422)
 		expect(res.body.message).to.equal(ErrorMessage.EMAIL_ALREADY_USED)
@@ -89,14 +75,7 @@ describe('register', () => {
 	})
 
 	it('should not register a new user with an invalid email', async () => {
-		const res = await chai.request(app)
-			.post('/api/auth/register')
-			.send({
-				username: 'authTest2',
-				email: 'invalidEmail',
-				password: STRONG_PASSWORD,
-				passwordConfirmation: STRONG_PASSWORD
-			})
+		const res = await register({ email: 'invalid_email' })
 
 		expect(res.status).to.equal(422)
 		expect(res.body.message).to.equal(ErrorMessage.INVALID_VALUES)
@@ -104,14 +83,10 @@ describe('register', () => {
 	})
 
 	it('should not register a new user with a weak password', async () => {
-		const res = await chai.request(app)
-			.post('/api/auth/register')
-			.send({
-				username: 'authTest2',
-				email: 'authTest2@test.com',
-				password: WEAK_PASSWORD,
-				passwordConfirmation: WEAK_PASSWORD
-			})
+		const res = await register({
+			password: 'weak_password',
+			passwordConfirmation: 'weak_password'
+		})
 
 		expect(res.status).to.equal(422)
 		expect(res.body.message).to.equal(ErrorMessage.INVALID_VALUES)
@@ -119,14 +94,7 @@ describe('register', () => {
 	})
 
 	it('should not register a new user with non matching passwords', async () => {
-		const res = await chai.request(app)
-			.post('/api/auth/register')
-			.send({
-				username: 'authTest2',
-				email: 'authTest2@test.com',
-				password: STRONG_PASSWORD,
-				passwordConfirmation: WEAK_PASSWORD
-			})
+		const res = await register({ passwordConfirmation: 'non_matching' })
 
 		expect(res.status).to.equal(422)
 		expect(res.body.message).to.equal(ErrorMessage.INVALID_VALUES)
@@ -136,53 +104,40 @@ describe('register', () => {
 
 describe('login', () => {
 	before(async () => {
-		await prisma.user.create({
-			data: {
-				username: 'authTest',
-				email: 'authTest@test.com',
-				password: await authService.hashPassword(STRONG_PASSWORD),
-				language: Language.en
-			}
-		})
+		await testService.createTestUser({ username: USERNAME, email: EMAIL })
 	})
 
 	after(async () => {
-		await prisma.user.deleteMany({ where: { username: 'authTest' } })
+		await prisma.user.deleteMany({ where: { username: USERNAME } })
 	})
 
 	it('should login a user', async () => {
-		const res = await chai.request(app)
-			.post('/api/auth/login')
-			.send({
-				username: 'authTest',
-				password: STRONG_PASSWORD
-			})
+		const res = await login({
+			username: USERNAME,
+			password: TEST_PASSWORD
+		})
 
 		expect(res.status).to.equal(200)
 		expect(res.body.token).to.be.a('string').and.to.match(/^.+\..+\..+$/)
 		expect(res.body.session.language).to.equal(Language.en)
-		expect(res.body.session.user.username).to.equal('authTest')
+		expect(res.body.session.user.username).to.equal(USERNAME)
 	})
 
 	it('should not login a user with a wrong password', async () => {
-		const res = await chai.request(app)
-			.post('/api/auth/login')
-			.send({
-				username: 'authTest',
-				password: WEAK_PASSWORD
-			})
+		const res = await login({
+			username: USERNAME,
+			password: 'wrong_password'
+		})
 
 		expect(res.status).to.equal(403)
 		expect(res.body.message).to.equal(ErrorMessage.INVALID_CREDENTIALS)
 	})
 
 	it('should not login a user with a wrong username', async () => {
-		const res = await chai.request(app)
-			.post('/api/auth/login')
-			.send({
-				username: 'wrongUsername',
-				password: STRONG_PASSWORD
-			})
+		const res = await login({
+			username: 'wrong_username',
+			password: TEST_PASSWORD
+		})
 
 		expect(res.status).to.equal(403)
 		expect(res.body.message).to.equal(ErrorMessage.INVALID_CREDENTIALS)
