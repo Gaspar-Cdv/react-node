436bf9cb2fc48d5a46588a7d331094a8910058e7
Gaspar Chefdeville <gaspar.chefdeville@gmail.com>
2022-09-14 22:59:48 +0200

    feat(server): create reset password service

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 import authRoute from './routes/authRoute'
+import tokenRoute from './routes/tokenRoute'
 import userRoute from './routes/userRoute'
 import { authenticated } from './middlewares/authenticated'
 
 const router = Router()
 
 router.use('/auth', authRoute)
+router.use('/token', tokenRoute)
 router.use('/user', authenticated, userRoute)
 
 export default router
